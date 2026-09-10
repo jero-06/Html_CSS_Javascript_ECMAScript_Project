656e5abd1534a147b9f22e490d72c44ab2938b33
jero-06 <jeeyoung345@gmail.com>
2026-09-10 11:16:08 +0900

    form.js add

--- student_js/js/form.js
+++ student_js/js/form.js
@@ -12,9 +12,14 @@ document.addEventListener("DOMContentLoaded", function () {
 
 // 폼 제출 이벤트 핸들러
 studentForm.addEventListener("submit", function (e) {
-  e.preventDefault();
-  const formData = new FormData(studentForm);
-  console.log(formData);
+    e.preventDefault();
+    const formData = new FormData(studentForm);
+    
+    console.log(Object.fromEntries(formData));
+    for (const [key, value] of formData.entries()) {
+        console.log(key, "=", value);
+    }
+
 });
 
 // 학생 목록 로드 함수
