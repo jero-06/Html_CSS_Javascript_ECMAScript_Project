b0db8420dae6c82b8668c37429804d5ab0c0c2db
jero-06 <jeeyoung345@gmail.com>
2026-09-10 14:27:46 +0900

    form.js renderStudentTable update

--- student_js/js/form.js
+++ student_js/js/form.js
@@ -94,10 +94,10 @@ function renderStudentTable(students) {
         row.innerHTML = `
                     <td>${student.name}</td>
                     <td>${student.studentNumber}</td>
-                    <td>${student.detail ? student.detail.address : "-"}</td>
-                    <td>${student.detail ? student.detail.phoneNumber : "-"}</td>
-                    <td>${student.detail ? student.detail.email || "-" : "-"}</td>
-                    <td>${student.detail ? student.detail.dateOfBirth || "-" : "-"}</td>
+                    <td>${student.detail?.address ?? "-"}</td>
+                    <td>${student.detail?.phoneNumber ?? "-"}</td>
+                    <td>${student.detail?.email || "-"}</td>
+                    <td>${student.detail?.dateOfBirth || "-"}</td>
                     <td>
                         <button class="edit-btn" onclick="editStudent(${student.id})">수정</button>
                         <button class="delete-btn" onclick="deleteStudent(${student.id})">삭제</button>
