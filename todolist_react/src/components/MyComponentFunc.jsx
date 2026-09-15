36d82dc36475b548066b270f39e3f53494fe474f
jero-06 <jeeyoung345@gmail.com>
2026-09-15 16:00:14 +0900

    MyComponentFunc update

---
+++ todolist_react/src/components/MyComponentFunc.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+const MyComponentFunc = ({ name, age }) => {
+    return (
+        <div>
+            <h2>함수 형태의 컴포넌트</h2>
+            <h3>Hello! {name} / {age}</h3>
+        </div>
+        );
+    }
+
+export default MyComponentFunc;
