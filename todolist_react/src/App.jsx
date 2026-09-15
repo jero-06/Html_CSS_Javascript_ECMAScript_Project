861ce842be74097dee9139fb8bdbe29024443fac
jero-06 <jeeyoung345@gmail.com>
2026-09-15 16:10:41 +0900

    MyComp.jsx update

--- todolist_react/src/App.jsx
+++ todolist_react/src/App.jsx
@@ -1,13 +1,16 @@
 import './App.css'
 import MyComponentFunc from './components/MyComponentFunc.jsx'
-import MyComponent from './MyComponent.jsx'
+import MyComponent from './components/MyComponent.jsx'
 
 function App() {
 
   return (
     <>
-      <MyComponent name="홍길동" age={20}/>
-      <MyComponentFunc name="함수형" age={30}/>
+      <MyComponent name="클래스형" age={20}/>
+      <hr/>
+      <MyComponentFunc name="함수형" age={30}>
+        <p>함수형의 하위 엘리먼트 P</p>
+      </MyComponentFunc>
     </>
   )
 }
