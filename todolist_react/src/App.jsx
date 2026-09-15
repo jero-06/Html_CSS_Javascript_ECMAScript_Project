66119ffdc137d0d3d0cef2c559eca24bf7e74615
jero-06 <jeeyoung345@gmail.com>
2026-09-15 15:58:03 +0900

    MyComponentFunc add

--- todolist_react/src/App.jsx
+++ todolist_react/src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import MyComponentFunc from './components/MyComponentFunc.jsx'
 import MyComponent from './MyComponent.jsx'
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
   return (
     <>
       <MyComponent name="홍길동" age={20}/>
+      <MyComponentFunc name="함수형" age={30}/>
     </>
   )
 }
