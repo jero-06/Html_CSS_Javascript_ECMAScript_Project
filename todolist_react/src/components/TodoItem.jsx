523718a62145f65a9224e5b8d92e95128fc45fc9
jero-06 <jeeyoung345@gmail.com>
2026-09-16 15:24:07 +0900

    TodoItem memo() add

--- todolist_react/src/components/TodoItem.jsx
+++ todolist_react/src/components/TodoItem.jsx
@@ -1,4 +1,5 @@
 import "./TodoItem.css";
+import { memo } from "react";
 
 const TodoItem = ({ text, checked, id, onToggle, onRemove }) => {
   return (
@@ -20,4 +21,8 @@ const TodoItem = ({ text, checked, id, onToggle, onRemove }) => {
   );
 };
 
-export default TodoItem;
+// export default TodoItem;
+export default memo(
+    TodoItem,
+    (prevProps, nextProps) => prevProps.checked === nextProps.checked
+);
