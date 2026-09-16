7480b696b8dbaf2ec2cc8a73b98dcfae021f14db
jero-06 <jeeyoung345@gmail.com>
2026-09-16 11:12:51 +0900

    TodoItemList => TodoItem.jsx call

--- todolist_react/src/components/TodoItemList.jsx
+++ todolist_react/src/components/TodoItemList.jsx
@@ -1,8 +1,13 @@
+import TodoItem from "./TodoItem";
+
 const TodoItemList = ({ myTodos, myToggle, myRemove }) => {
-    return (
-        <div>
-        </div>
-    );
+  return (
+    <div>
+      <TodoItem text="안녕" />
+      <TodoItem text="리액트" />
+      <TodoItem text="반가워" />
+    </div>
+  );
 };
- 
+
 export default TodoItemList;
