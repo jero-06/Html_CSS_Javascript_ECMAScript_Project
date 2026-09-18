f8f83e39ff07baba391d324bb1e61cf72a69c1ac
jero-06 <jeeyoung345@gmail.com>
2026-09-19 01:37:57 +0900

    main.js update

--- book_ecma/src/api/bookApi.js
+++ book_ecma/src/api/bookApi.js
@@ -1,14 +1,5 @@
 import { BOOKS_URL, JSON_HEADERS } from "../config.js";
 
-export async function fetchBooks() {
-  const response = await fetch(BOOKS_URL);
-
-  if (!response.ok) {
-    throw new Error("도서 목록을 불러오는데 실패했습니다.");
-  }
-  return response.json();
-}
-
 async function request(url, options = {}) {
   const response = await fetch(url, options);
   if (!response.ok) {
