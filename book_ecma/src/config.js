9091a9108ccc2ac60ad7d0e72d819ef0d6f92378
jero-06 <jeeyoung345@gmail.com>
2026-09-19 00:13:59 +0900

    config.js add

---
+++ book_ecma/src/config.js
@@ -0,0 +1,7 @@
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL ?? "http://localhost:8080";
+
+export const BOOKS_URL = '${API_BASE_URL}/api/books';   // API_BASE_URL 뒤에 /api/books 를 붙인 문자열
+
+export const JSON_HEADERS = {
+  ???
+};
