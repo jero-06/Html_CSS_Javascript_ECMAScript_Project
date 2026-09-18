9ec4cc30ca1e1cb2a32eb9015f26117531d2623f
jero-06 <jeeyoung345@gmail.com>
2026-09-18 14:10:35 +0900

    StudentListPage.jsx add

--- student_react_router_axios/src/main.jsx
+++ student_react_router_axios/src/main.jsx
@@ -1,11 +1,11 @@
-import { StrictMode } from 'react'
+//import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowerRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <BrowerRouter>
+  <BrowserRouter>
     <App />
-  </BrowerRouter>,
+  </BrowserRouter>,
 )
